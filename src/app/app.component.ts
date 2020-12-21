2908e24ba0d97812ddb7e25fc064923c2fdf0e1c
unknown <unenes.viejos@gmail.com>
2020-12-20 22:29:51 -0600

    se agregaron estilos de sombreado y se implemento la funcion en el TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Piano-Angular';
+  title = 'Piano';
+
+  // Funcion AplicarSonido
+  aplicarSonido(numero: number): void{
+
+    const audio = new Audio();
+    // se concatena el numero para hacer referencia a alguno de los sonidos guardados en esa ruta 
+    // Ejemplo: note1.wav donde 1 esta vinculado a la variable numero recivida por la funcion
+    audio.src = '../assets/Sonidos/note'+numero+'.wav';
+    // Cargar audio
+    audio.load();
+    // Play audio
+    audio.play();
+
+  }
 }
